4f59d930e0997316e575d3d4700efb450d1bfa32
Irelia <yaokai729@outlook.com>
2020-11-13 22:53:13 +0800

    完成TagsSection部分jss

--- src/components/TagsSection.tsx
+++ src/components/TagsSection.tsx
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const TagsSection = styled.section`
- background-color:#fff;
+
+const Wrapper = styled.section`
+    background-color:#fff;
     padding: 12px 16px;
     flex-grow:1;
     display:flex;
@@ -17,6 +19,9 @@ const TagsSection = styled.section`
       border-radius:18px;
       padding: 3px 17px;
       font-size:14px;
+      &.selected{
+        background-color:green
+      }
     }
   }
   >button{
@@ -28,4 +33,41 @@ const TagsSection = styled.section`
     color:#666;
   }
 `
+
+const TagsSection:React.FC=(props)=>{
+  const [tags,setTags] = useState<string[]>(['衣','食','住','行'])
+  const [selectedTags,setSelectedTag] = useState<string[]>([])
+
+  const onAddTag= ()=>{
+    const tagName = window.prompt('新标签的名称为')
+    console.log(`---${tagName}---`);
+    if(tagName === ''){
+      alert('标签名不能为空')
+      return false;
+    }
+    if(tagName !== null){
+      setTags([...tags, tagName])
+    }
+  }
+  const onToggleTag = (tag:string)=>{
+    const index = selectedTags.indexOf(tag)
+    if(index < 0){
+      setSelectedTag([...selectedTags, tag])
+    }else{
+      setSelectedTag(selectedTags.filter(t=>t!==tag))      
+    }
+  }
+  const getClass = (tag:string)=> selectedTags.indexOf(tag)>=0?'selected':''
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag=>
+          <li key={tag} onClick={()=>{onToggleTag(tag)}} className={getClass(tag)}>{tag}</li>
+          )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  )
+}
+
 export {TagsSection}
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -17,15 +17,7 @@ const MyLayout = styled(Layout)`
 function Tags() {
   return (
     <MyLayout>
-       <TagsSection>
-        <ol>
-          <li>衣</li> 
-          <li>食</li>  
-          <li>住</li> 
-          <li>行</li> 
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
+       <TagsSection></TagsSection>
         <NotesSection>
           <label htmlFor="">
             <span>备注</span>
